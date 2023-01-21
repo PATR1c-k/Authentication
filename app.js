aec5e3746a33caa52eeeabbd6bf7276fa2bd26cf
Pratik <pratikkaple1nov2002@gmail.com>
2023-01-21 12:26:13 +0530

    Hashing(level 3)

--- app.js
+++ app.js
@@ -4,8 +4,7 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const { urlencoded } = require("body-parser");
 const { default: mongoose } = require("mongoose");
-const encrypt = require("mongoose-encryption");
-
+const md5 = require("md5");
 
 const app = express();
 
@@ -21,9 +20,6 @@ const userSchema = new mongoose.Schema({
     email: String,
     password: String
 });
-
-
-userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"] });
 
 const User = new mongoose.model("user", userSchema);
 
@@ -43,7 +39,7 @@ app.get("/register", function (req, res) {
 app.post("/register", function (req, res) {
     const newUser = new User({
         email: req.body.username,
-        password: req.body.password
+        password: md5(req.body.password)
     });
     newUser.save(function (err) {
         if (err) {
@@ -54,9 +50,11 @@ app.post("/register", function (req, res) {
     })
 })
 
+console.log(md5("Pratik"));
+
 app.post("/login", function (req, res) {
     const username = req.body.username;
-    const password = req.body.password;
+    const password = md5(req.body.password);
 
     User.findOne({ email: username }, function (err, foundUser) {
         if (!err) {
