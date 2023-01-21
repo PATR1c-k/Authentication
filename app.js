20bc5a9f27b6b31404ed630ae6246966dc4835d5
Pratik <pratikkaple1nov2002@gmail.com>
2023-01-21 11:55:32 +0530

    add envirnoment variables

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
@@ -13,17 +14,16 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 
 mongoose.set('strictQuery', false);
-mongoose.connect("mongodb+srv://pratik:test123@cluster0.am5xmqs.mongodb.net/UserDB", { useNewUrlParser: true });
+
+mongoose.connect(process.env.API_KEY, { useNewUrlParser: true });
 
 const userSchema = new mongoose.Schema({
     email: String,
     password: String
 });
 
-const secret = "Thisislegendpratik";
-userSchema.plugin(encrypt, { secret: secret, encryptedFields: ["password"] });
 
-
+userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"] });
 
 const User = new mongoose.model("user", userSchema);
 
